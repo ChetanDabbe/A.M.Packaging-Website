7baa54fb13d494709f8d00cfefa53da32c376019
ChetanDabbe <160775990+ChetanDabbe@users.noreply.github.com>
2025-01-12 18:56:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(
     credentials: true,
   })
 );
-
+console.log(process.env.FRONTEND_URI);
 // app.use(
 //   cors({
 //     // origin: "http://localhost:3000", // Replace with your frontend URL
